45e722fa958c45fb10eb0069ad8a735a96772a2a
radish.zhou <lxfeif123@126.com>
2020-06-28 20:05:27 +0800

    gin insert

---
+++ gin/databases/mysql.go
@@ -0,0 +1,16 @@
+package databases
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func GetDb() *sql.DB {
+	db, err := sql.Open("mysql", "root:123456@tcp(localhost:3306)/test")
+	if err != nil {
+		panic(err.Error())
+		fmt.Print("mysql 链接错误")
+	}
+	return db
+}
